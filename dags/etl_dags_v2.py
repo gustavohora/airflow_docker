c333ed9d1d860b69c5bd835ba980b9836faddab6
gustavohora <gustavo_hora@live.com>
2023-02-18 21:50:41 -0300

    sinc

---
+++ dags/etl_dags_v2.py
@@ -0,0 +1,51 @@
+#intalar bibliotecas
+#pip install -r requirements.txt
+
+#baixando as bibliotecas
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.bash import BashOperator
+import pandas as pd
+import requests
+import json
+
+def captura_conta_dados():
+        url = 'https://data.cityofnewyork.us/resource/rc75-m7u3.json'
+        response = requests.get(url)
+        df = pd.DataFrame(json.loads(response.content))
+        qtd = len(df.index)
+        return qtd
+
+def e_valida(ti):
+        qtd = ti.xcom_pull(task_ids = 'captura_conta_dados')
+        if (qtd > 10):
+                return 'valido'
+        return 'nao_valido'
+
+with DAG('etl_dag_v2', start_date = datetime(2023, 1, 1),
+        schedule = '30 * * * *',
+        catchup = False
+        ) as dag:
+
+     captura_conta_dados = PythonOperator(
+        task_id = 'captura_conta_dados',
+        python_callable = captura_conta_dados
+     )
+
+     e_valida = BranchPythonOperator(
+        task_id = 'e_valida',
+        python_callable = e_valida
+     )
+
+     valido = BashOperator (
+        task_id = 'valido',
+        bash_command = "echo 'Quantidade Ok'"
+     )
+
+     nvalido = BashOperator (
+        task_id = 'nao_valido',
+        bash_command = "echo 'Quantidade Não Ok'"
+     )
+
+captura_conta_dados >> e_valida >> [valido, nvalido]
